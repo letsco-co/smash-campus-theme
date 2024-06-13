15c9b961f991c83ebb9dee2b8161a8b7ea68035b
Auriane <auriane.chay@letsco.co>
2024-06-13 15:35:20 +0200

    feat: display speakers

--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -1,4 +1,14 @@
-new Glide('.glide', {
+new Glide('#glideImages', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+    gap: 100,
+    keyboard: true,
+    focusAt: 'center',
+    peek: 100,
+}).mount()
+
+new Glide('#glideSpeakers', {
     type: 'carousel',
     startAt: 0,
     perView: 3,
